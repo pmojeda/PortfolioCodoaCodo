960ad052a4f9dd9fb3a659be63195538919f606f
Martin Ojeda <pmojeda@gmail.com>
2022-09-14 14:01:30 -0300

    Primer commit

---
+++ header.js
@@ -0,0 +1,14 @@
+document.write('<header> ' +
+    '<div class="LogoTexto">' +
+    '    <p>Portfolio</p> ' +
+    '</div>' +
+    '<nav class="NavBar">' +
+    '    <ul>' +
+    '        <li><a href="#">Inicio</a></li>' +
+    '        <li><a href="#">Acerca de mi</a></li>' +
+    '        <li><a href="#">Servicios</a></li>' +
+    '        <li><a href="#">Portfolio</a></li>' +
+    '        <li><a href="#">Contacto</a></li>' +
+    '    </ul>' +
+    '</nav>' +
+'</header>');
