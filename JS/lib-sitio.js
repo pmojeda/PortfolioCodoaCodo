7534aef447fc965d3da9074453320b1cdf7a3d00
pmojeda <pmojeda@gmail.com>
2022-09-26 18:48:53 -0300

    Agregado de menú mobile y break points con media query

---
+++ JS/lib-sitio.js
@@ -0,0 +1,9 @@
+function activarMenuMobile() {
+    var x = document.getElementById("misItemsMobile");
+
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
