db092345b5588bdcb8e58fc02d3d87b1f2d890da
pmojeda <pmojeda@gmail.com>
2022-09-28 11:20:59 -0300

    renombrado de clases

--- JS/lib-sitio.js
+++ JS/lib-sitio.js
@@ -1,5 +1,5 @@
 function activarMenuMobile() {
-    var x = document.getElementById("misItemsMobile");
+    var x = document.getElementById("itemsMobile");
 
     if (x.style.display === "block") {
       x.style.display = "none";
